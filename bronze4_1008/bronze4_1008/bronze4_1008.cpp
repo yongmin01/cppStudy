cc3fd3816d98de09303bf6d7b4c3525b0d5819e4
yongmin01 <78396941+yongmin01@users.noreply.github.com>
2021-07-05 04:24:09 +0900

    bj bronze4-1008
    반년만에 c++하니까 백지가 됐다. 오늘부터 시작~~~~

---
+++ bronze4_1008/bronze4_1008/bronze4_1008.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A;
+    int B;
+    while (true) {
+        cin >> A >> B;
+        if (0 < A && A < 10) {
+            if (0 < B && B < 10) {
+                break;
+            }
+        }
+    }
+    cout.precision(10);
+    cout << (double)A / B;
+    return 0;
+}
