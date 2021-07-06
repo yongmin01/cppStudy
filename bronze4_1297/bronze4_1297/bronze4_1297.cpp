87852cd0141cf9bdc374caa5979fc85c2223a13a
yongmin01 <78396941+yongmin01@users.noreply.github.com>
2021-07-06 23:30:41 +0900

    bj bronze4-1297
    커밋하려고 너무 쉬운거 풀음;

---
+++ bronze4_1297/bronze4_1297/bronze4_1297.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int minkook[4];
+    int mansae[4];
+
+    int S = 0;
+    int T = 0;
+
+    for (int i = 0; i <= 3; ++i) {
+        cin >> minkook[i];
+    }
+
+    for (int i = 0; i <= 3; ++i) {
+        cin >> mansae[i];
+    }
+
+    for (int i = 0; i <= 3; ++i) {
+        S += minkook[i];
+        T += mansae[i];
+    }
+
+    if (S >= T) {
+        cout << S;
+    }
+    else cout << T;
+
+    return 0;
+}
+
+
