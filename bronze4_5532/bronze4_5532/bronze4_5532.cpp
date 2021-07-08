3f6a15711764f734af8618a2500c5322a9cd0ac3
yongmin01 <78396941+yongmin01@users.noreply.github.com>
2021-07-08 23:35:24 +0900

    bronze4-5532
    너무 비효율적인거 같은디.. 생각하기 귀찮다^~^

---
+++ bronze4_5532/bronze4_5532/bronze4_5532.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{   
+    int L;
+    int A;
+    int B;
+    int C;
+    int D;
+
+    int m_mok = 0;
+    int k_mok = 0;
+
+    
+    
+    cin >> L;
+    while (L < 2 || L > 40) {
+        cin >> L;
+    }
+
+    cin >> A;
+    while (A < 1 || A > 1000) {
+        cin >> A;
+    }
+
+    cin >> B;
+    while (B < 1 || B > 1000) {
+        cin >> B;
+    }
+
+    cin >> C;
+    while (C < 1 || C > 100) {
+        cin >> A;
+    }
+
+    cin >> D;
+    while (D < 1 || D > 100) {
+        cin >> D;
+    }
+
+    if (B % D == 0) {
+        m_mok += B / D;
+    }
+    else m_mok = B / D + 1;
+
+    if (A % C == 0) {
+        k_mok += A / C;
+    }
+    else k_mok = A / C + 1;
+
+    cout << L - max(m_mok, k_mok);
+
+    /*
+    cin >> L;
+    cin >> A;
+    cin >> B;
+    cin >> C;
+    cin >> D;
+
+    if (2 <= L <= 40) {
+        if (1 <= A <= 1000) {
+            if (1 <= B <= 1000) {
+                if (1 <= C <= 100) {
+                    if (1 <= D <= 100) {
+                        if (B %)
+                    }
+                }
+            }
+        }
+    }
+    */
+}
