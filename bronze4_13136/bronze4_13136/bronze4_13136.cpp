9c8f21d2089f657b797e3be6e296bb0cb3562e76
yongmin01 <78396941+yongmin01@users.noreply.github.com>
2021-07-09 16:10:17 +0900

    bronze4-13136
    단위 주의할것 !!
    int -> long

---
+++ bronze4_13136/bronze4_13136/bronze4_13136.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    long R;
+    long C;
+    long N;
+
+    cin >> R >> C >> N;
+    while (R < 1 || R > 1000000 || C < 1 || C > 1000000 || N < 1 || N > 1000000) {
+        cin >> R >> C >> N;
+    }
+
+    long hang;
+    long yeaol;
+    
+    if (R % N == 0) {
+        hang = R / N;
+    }
+    else hang = R / N + 1;
+    
+    if (C % N == 0) {
+        yeaol = C / N;
+    }
+    else yeaol = C / N + 1;
+    
+
+    cout << hang * yeaol;
+}
