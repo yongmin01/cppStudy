2139742750fca5a9b4291e515b9e98c71daada97
yongmin01 <78396941+yongmin01@users.noreply.github.com>
2021-07-11 23:38:21 +0900

    bronze4-2530(complete)
    한방에 맞춰따~~

--- bronze4_2530/bronze4_2530/bronze4_2530.cpp
+++ bronze4_2530/bronze4_2530/bronze4_2530.cpp
@@ -18,22 +18,63 @@ int main()
     else if (C < 0 || C > 59) {
         cin >> A >> B >> C;
     }
-        
+
     cin >> D;
     while (D < 0 || D > 500000) {
         cin >> D;
     }
 
+
     if (D / 86400 > 0) {
         D = D % 86400;
         if (D / 3600 > 0) {
             A = A + (D / 3600);
-            if (A > 23) {
-                A -= 24;
+            D = D % 3600;
+            if (D > 60) {
+                B = B + (D / 60);
+                D = D % 60;
+                C = C + D;
             }
+            else C = C + D;
         }
-        else if (D )
+        else if (D / 60 > 0) {
+            B = B + (D / 60);
+            D = D % 60;
+            C = C + D;
+        }
+        else C = C + D;
     }
+    else if (D / 3600 > 0) {
+        A = A + (D / 3600);
+        D = D % 3600;
+        if (D > 60) {
+            B = B + (D / 60);
+            D = D % 60;
+            C = C + D;
+        }
+        else C = C + D;
+    }
+    else if (D / 60 > 0) {
+        B = B + (D / 60);
+        D = D % 60;
+        C = C + D;
+    }
+    else C = C + D;
+
+
+    if (C > 59) {
+        C = C - 60;
+        B = B + 1;
+    }
+    if (B > 59) {
+        B = B - 60;
+        A = A + 1;
+    }
+    if (A > 23) {
+        A = A - 24;
+    }
+    cout << A << ' ' << B << ' ' << C;
+}
 
 
 
