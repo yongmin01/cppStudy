9aae26866c54c43cb15c2133d7dcecab15569024
yongmin01 <78396941+yongmin01@users.noreply.github.com>
2021-07-12 23:38:33 +0900

    bronze4-13866
    차이 = 절대값

---
+++ bronze4_13866/bronze4_13866/bronze4_13866.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int A;
+	int B;
+	int C;
+	int D;
+	int R;
+	
+	cin >> A >> B >> C >> D;
+	while (true) {
+		if (A >= 0 && A <= 10000 && B >= 0 && B <= 10000 && C >= 0 && C <= 10000 && D >= 0 && D <= 10000) {
+			if (A <= B && B <= C && C <= D) {
+				break;
+			}
+		}
+		cin >> A >> B >> C >> D;
+	}
+
+	R = (A + D) - (B + C);
+	if (R >= 0) {
+		cout << R;
+	}
+	else cout << -1 * R;
+}
