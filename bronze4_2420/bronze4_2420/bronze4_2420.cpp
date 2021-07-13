df609efd17dedfa47d3998833cbd288a046e988c
yongmin01 <78396941+yongmin01@users.noreply.github.com>
2021-07-13 18:51:12 +0900

    bronze4-2420

---
+++ bronze4_2420/bronze4_2420/bronze4_2420.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    long N;
+    long M;
+
+    cin >> N >> M;
+    while (N < -2000000000 || N > 2000000000 || M < -2000000000 || M > 2000000000) {
+        cin >> N >> M;
+    }
+
+    if ((N - M) >= 0) {
+        cout << N - M;
+    }
+    else cout << -1 * (N - M);
+}
