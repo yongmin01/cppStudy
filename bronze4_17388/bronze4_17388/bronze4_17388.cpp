891f08bc01963b907219f55387b96340afd15721
yongmin01 <78396941+yongmin01@users.noreply.github.com>
2021-07-14 21:12:42 +0900

    bronze4-17388

---
+++ bronze4_17388/bronze4_17388/bronze4_17388.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int S;
+	int K;
+	int H;
+
+	cin >> S >> K >> H;
+	while (true) {
+		if (S < 0 || S > 100) {
+			cin >> S >> K >> H;
+		}
+		else if (K < 0 || K > 100) {
+			cin >> S >> K >> H;
+		}
+		else if (H < 0 || H > 100) {
+			cin >> S >> K >> H;
+		}
+		else if (S == K || S == H || K == H) {
+			cin >> S >> K >> H;
+		}
+		else break;
+	}
+
+	if (S + K + H >= 100) {
+		cout << "OK";
+	}
+	else {
+		if (S < K && S < H) {
+			cout << "Soongsil";
+		}
+		else if (K < S && K < H) {
+			cout << "Korea";
+		}
+		else cout << "Hanyang";
+	}
+
+	return 0;
+}
