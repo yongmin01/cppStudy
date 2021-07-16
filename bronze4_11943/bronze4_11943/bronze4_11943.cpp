9d2c50f10795b4073bf4e879ec5689972ba761ab
yongmin01 <78396941+yongmin01@users.noreply.github.com>
2021-07-16 22:49:56 +0900

    bronze4-11943
    break 까먹지말기!
    변수 한줄에 정의하기 ok

---
+++ bronze4_11943/bronze4_11943/bronze4_11943.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int A, B, C, D;
+
+	cin >> A >> B;
+	while (true) {
+		if (A < 0 || A > 1000) {
+			cin >> A >> B;
+		}
+		else if (B < 0 || B > 1000) {
+			cin >> A >> B;
+		}
+		else break;
+	}
+	cin >> C >> D;
+	while (true) {
+		if (C < 0 || C > 1000) {
+			cin >> C >> D;
+		}
+		else if (D < 0 || D > 1000) {
+			cin >> C >> D;
+		}
+		else break;
+	}
+
+	int sol1 = A + D;
+	int sol2 = B + C;
+
+	if (sol1 >= sol2) {
+		cout << sol2;
+	}
+	else cout << sol1;
+
+	return 0;
+}
