597dd20035868d9c2f19dd6f7f24891da3815c12
yongmin01 <78396941+yongmin01@users.noreply.github.com>
2021-07-17 23:46:04 +0900

    bronze4-16486
    오차범위 이상하게 품! -다시 공부할것

---
+++ bronze4_16486/bronze4_16486/bronze4_16486.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	unsigned int d1, d2;
+	float p = 3.141592f;
+
+	cin >> d1;
+	while (true) {
+		if (d1 <= 0 || d1 > 100000) {
+			cin >> d1;
+		}
+		else break;
+	}
+	cin >> d2;
+	while (true) {
+		if (d2 <= 0 || d2 > 100000) {
+			cin >> d2;
+		}
+		else break;
+	}
+
+	float result = (d1 * 2) + (d2 * 2 * p);
+	cout.precision(8);
+	cout << result;
+
+	return 0;
+
+}
