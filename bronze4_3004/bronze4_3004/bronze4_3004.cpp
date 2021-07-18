9695ba15f1011790f804a9c906cd4f16abdb9a08
yongmin01 <78396941+yongmin01@users.noreply.github.com>
2021-07-18 23:45:02 +0900

    bronze4-3004

---
+++ bronze4_3004/bronze4_3004/bronze4_3004.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	unsigned int N;
+	cin >> N;
+	while (true) {
+		if (N < 1 || N > 100) {
+			cin >> N;
+		}
+		else break;
+	}
+
+	if (N % 2 == 0) {
+		cout << (N - (N / 2 - 1)) * (N - (N / 2 - 1));
+
+	}
+	else {
+		N += 1;
+		cout << (N - (N / 2 - 1)) * (N - (N / 2 - 1) - 1);
+	}
+
+	return 0;
+
+}
