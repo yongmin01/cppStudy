0ef20b337e8a023463af31dfb9e50eac81a054de
yongmin01 <78396941+yongmin01@users.noreply.github.com>
2021-07-19 23:40:37 +0900

    bronze4-19944

---
+++ bronze4_19944/bronze4_19944/bronze4_19944.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	unsigned int N, M;
+	cin >> N >> M;
+	while (true) {
+		if (N < 3 || N > 1000) {
+			cin >> N >> M;
+		}
+		else if (M < 1 || N > 1000) {
+			cin >> N >> M;
+		}
+		else break;
+	}
+	if (M <= 2) {
+		cout << "NEWBIE!";
+	}
+	else if (M <= N) {
+		cout << "OLDBIE!";
+	}
+	else cout << "TLE!";
+	
+	return 0;
+}
