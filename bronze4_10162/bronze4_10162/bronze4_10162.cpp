3882f628321779907e36310578b55104a9aa831e
yongmin01 <78396941+yongmin01@users.noreply.github.com>
2021-07-20 23:50:10 +0900

    bronze4-10162
    조건문 쓸 때 빠지는 경우 안 생기는지 확인!!

---
+++ bronze4_10162/bronze4_10162/bronze4_10162.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	while (true) {
+		if (T < 1 || T > 10000) {
+			cin >> T;
+		}
+		else break;
+	}
+	unsigned int A = 0;
+	unsigned int B = 0;
+	unsigned int C = 0;
+
+	if (T % 10 == 0) {
+		if (T >= 300) {
+			A = T / 300;
+			T = T % 300;
+			if (T >= 60) {
+				B = T / 60;
+				T = T % 60;
+				if (T >= 10) {
+					C = T / 10;
+				}
+			}
+			else if (T >= 10) {
+				C = T / 10;
+			}
+		}
+		else if (T >= 60) {
+			B = T / 60;
+			T = T % 60;
+			if (T >= 10) {
+				C = T / 10;
+			}
+		}
+		else if (T >= 10) {
+			C = T / 10;
+		}
+		cout << A << " " << B << " " << C;
+	}
+	else cout << "-1";
+
+	return 0;
+}
