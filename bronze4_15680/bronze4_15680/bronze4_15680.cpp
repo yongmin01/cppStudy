a49a87b86e5d67c42942a6e4407a6940e2433cea
yongmin01 <78396941+yongmin01@users.noreply.github.com>
2021-07-21 23:20:55 +0900

    bronze4-15680
    시간 써서 어려운것 좀 풀자..

---
+++ bronze4_15680/bronze4_15680/bronze4_15680.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	while (true) {
+		if (N == 0 || N == 1) {
+			break;
+		}
+		else cin >> N;
+	}
+	if (N == 0) {
+		cout << "YONSEI";
+	}
+	else if (N == 1) {
+		cout << "Leading the Way to the Future";
+	}
+	
+	return 0;
+}
