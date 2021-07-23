f3ce7b472437292d29800b8fd63b1e6f31d2cc6e
yongmin01 <78396941+yongmin01@users.noreply.github.com>
2021-07-23 23:17:41 +0900

    bronze4-10101
    비교 연산자 두 항 이상 비교  불가

---
+++ bronze4_10101/bronze4_10101/bronze4_10101.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	unsigned int a, b, c;
+	cin >> a;
+	while (true) {
+		if (a <= 0 || a >= 180) {
+			cin >> a;
+		}
+		else break;
+	}
+
+	cin >> b;
+	while (true) {
+		if (b <= 0 || b >= 180) {
+			cin >> b;
+		}
+		else break;
+	}
+
+	cin >> c;
+	while (true) {
+		if (c <= 0 || c >= 180) {
+			cin >> c;
+		}
+		else break;
+	}
+
+	if (a == 60 && b == 60 && c == 60) {
+		cout << "Equilateral";
+	}
+	else if (a + b + c == 180) {
+		if (a == b || b == c || c == a) {
+			cout << "Isosceles";
+		}
+		else if (a != b && b != c && c != a) {
+			cout << "Scalene";
+		}
+	}
+	else cout << "Error";
+
+	return 0;
+}
