c57b398d7961e1d3ba5cc3b089d712cf5e1e5071
yongmin01 <78396941+yongmin01@users.noreply.github.com>
2021-07-23 23:20:34 +0900

    bronze3-1009
    배열 공부하고 다시 풀어보기

---
+++ bronze3_1009/bronze3_1009/bronze3_1009.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+	unsigned long int T, a, b;
+	cin >> T;
+	for (int i = 0; i < T; i++) {
+		cin >> a >> b >> "\n";
+		if (a < 1 || a > 1000000) {
+			cin >> a >> b;
+		}
+		else if (b < 1 || b > 1000000) {
+			cin >> a >> b;
+		}
+		cout << (unsigned long int)pow(a, b) % 10 << "\n";
+	}
+	
+	return 0;
+}
