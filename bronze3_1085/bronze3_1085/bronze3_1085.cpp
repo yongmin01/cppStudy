d64836b825992fef3a884f115a41eb787021d336
yongmin01 <78396941+yongmin01@users.noreply.github.com>
2021-07-26 23:48:05 +0900

    bronze3-1085

---
+++ bronze3_1085/bronze3_1085/bronze3_1085.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	unsigned int w, h, x, y;
+	cin >> x >> y >> w >> h;
+
+	int distance[4];
+	distance[0] = x;
+	distance[1] = w - x;
+	distance[2] = h - y;
+	distance[3] = y;
+	
+	cout << min({ distance[0], distance[1], distance[2], distance[3] });
+
+	return 0;
+}
