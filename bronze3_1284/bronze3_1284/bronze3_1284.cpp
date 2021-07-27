08d777b0a75497840eca7d83a44ff50815db580a
yongmin01 <78396941+yongmin01@users.noreply.github.com>
2021-07-27 22:47:57 +0900

    bronze3-1284
    string 사용함

---
+++ bronze3_1284/bronze3_1284/bronze3_1284.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	while (true) {
+		string N;
+		cin >> N;
+		int result = 0;
+		if (N == "0") {
+			break;
+		}
+		else {
+			for (int i = 0; i < N.length(); i++) {
+				if (N[i] == '0') {
+					result += 4;
+				}
+				else if (N[i] == '1') {
+					result += 2;
+				}
+				else result += 3;
+			}
+			cout << result + 2 + (N.length() - 1) << "\n";
+		}
+	}
+	return 0;
+}
