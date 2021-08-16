4e7d2c5f04d301e488e675f7dff1872a7ad28f81
yongmin01 <78396941+yongmin01@users.noreply.github.com>
2021-08-16 14:02:02 +0900

    brainCpp_ch15

--- brainCpp_ch15/brainCpp_ch15/brainCpp_ch15.cpp
+++ brainCpp_ch15/brainCpp_ch15/brainCpp_ch15.cpp
@@ -33,7 +33,7 @@ void Convert2Bin(int dec)
 
 	cout << dec % 2;
 }
-*/
+
 // 예제 15-5 함수의 포인터 기본
 void Dog()
 {
@@ -43,6 +43,20 @@ void Cat()
 {
 	cout << "I'm a cat.\n";
 }
+
+// 예제 15-6 자기 스스로 환경을 비교하는 경우
+void ForWindows9x();
+void ForWindowsNT();
+bool IsWindows9x();
+void ImportantFunc();
+*/
+//  예제 15-7 함수의 포인터를 사용한 개선
+typedef void (*SYSTEM_FUNC) ();
+
+void ForWindows9x();
+void ForWindowsNT();
+bool IsWindows9x();
+void ImportantFunc(SYSTEM_FUNC pfnSyst);
 
 
 
@@ -60,7 +74,7 @@ int main() {
 	// 예제 15-4 재귀 호출을 이용한 2진수로의 변환
 	Convert2Bin(13);
 	cout << "\n";
-	*/
+	
 	// 예제 15-5 함수의 포인터 기본
 	void(*p) ();
 
@@ -68,6 +82,57 @@ int main() {
 	(*p)();
 	p = &Cat;
 	(*p)();
+	
+	// 예제 15-6 자기 스스로 환경을 비교하는 경우
+	ImportantFunc();
+	*/
+	//  예제 15-7 함수의 포인터를 사용한 개선
+	SYSTEM_FUNC pfn;
+
+	if (IsWindows9x())
+		pfn = &ForWindows9x;
+	else
+		pfn = &ForWindowsNT;
+
+	ImportantFunc(pfn);
 
 	return 0;
+}
+
+
+// 예제 15-6 자기 스스로 환경을 비교하는 경우, 15-7 함수의 포인터를 사용한 개선
+void ForWindows9x()
+{
+	cout << "Windows 9x용 함수가 호출됨\n";
+}
+
+void ForWindowsNT()
+{
+	cout << "Windows NT/2000/XP용 함수가 호출됨\n";
+}
+
+bool IsWindows9x()
+{
+	return false;
+}
+/*
+void ImportantFunc()
+{
+	if (IsWindows9x())
+		ForWindows9x();
+	else
+		ForWindowsNT();
+
+	if (IsWindows9x())
+		ForWindows9x();
+	else
+		ForWindowsNT();
+}
+*/
+// 예제 15-7 함수의 포인터를 사용한 개선
+void ImportantFunc(SYSTEM_FUNC pfnSyst)
+{
+	(*pfnSyst) ();
+
+	(*pfnSyst) ();
 }
