a6e9d5148809467680f06cc0e0d190842c09fda9
yongmin01 <78396941+yongmin01@users.noreply.github.com>
2021-08-22 19:40:45 +0900

    brainCpp_ch21-9~14
    초기화가 필요한 클래스 - 초기화 리스트

---
+++ brainCpp_ch21-9/brainCpp_ch21-9/brainCpp_ch21-9.cpp
@@ -0,0 +1,39 @@
+﻿# include <iostream>
+using namespace std;
+
+class NeedConstructor
+{
+public:
+	const int maxCount;
+	int& ref;
+	int sample;
+
+	NeedConstructor();
+	NeedConstructor(int count, int& number);
+};
+
+NeedConstructor::NeedConstructor(int count, int& number)
+	: maxCount(count), ref(number)
+{
+	sample = 200;
+}
+
+NeedConstructor::NeedConstructor()
+	: maxCount(100), ref(sample)
+{
+	sample = 200;
+}
+
+
+int main()
+{
+	int number = 400;
+
+	NeedConstructor cr(300, number);
+
+
+	cout << "cr.maxCount = " << cr.maxCount << "\n";
+	cout << "cr.ref = " << cr.ref << "\n";
+
+	return 0;
+}
