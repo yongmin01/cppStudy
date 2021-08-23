26e21ed4f48a45cab3d81856df0479ec54d14c01
yongmin01 <78396941+yongmin01@users.noreply.github.com>
2021-08-23 16:45:03 +0900

    Update brainCpp_ch21-1.cpp

--- brainCpp_ch21-1/brainCpp_ch21-1/brainCpp_ch21-1.cpp
+++ brainCpp_ch21-1/brainCpp_ch21-1/brainCpp_ch21-1.cpp
@@ -4,14 +4,39 @@ using namespace std;
 class Point
 {
 public:
-	int x, y;
-
+	// 멤버 함수
 	void Print();
 
+	// 생성자들
 	Point();
 	Point(int initialX, int initialY);
 	Point(const Point& pt);
 
+	// 접근자
+	void SetX(int value)
+	{
+		if (value < 0)
+			x = 0;
+		else if (value > 100)
+			x = 100;
+		else
+			x = value;
+	}
+	void SetY(int value) 
+	{
+		if (value < 0)
+			y = 0;
+		else if (value > 100)
+			y = 100;
+		else
+			y = value;
+	}
+	int GetX() { return x; };
+	int GetY() { return y; };
+
+private:
+	// 멤버 변수
+	int x, y;
 };
 
 Point::Point(const Point& pt)
@@ -23,8 +48,8 @@ Point::Point(const Point& pt)
 
 Point::Point(int initialX, int initialY)
 {
-	x = initialX;
-	y = initialY;
+	SetX(initialX);
+	SetY(initialY);
 }
 Point::Point()
 {
@@ -36,23 +61,27 @@ void Point::Print()
 {
 	cout << "( " << x << ", " << y << ")\n";
 }
-
+/*
 void Area(const Point& pt)
 {
 	int area = pt.x * pt.y;
 
 	cout << "(0, 0)과 이루는 사각형의 면적 = " << area << "\n";
 }
+*/
 int main()
 {
-	int x = 5;
-	int y = 7;
+	Point pt(-50, 200);
+	pt.Print();
+
+	// int x = 5;
+	// int y = 7;
 
 	//예제 21-13 새로운 객체를 만들어서 전달하기
 	// Point temp(x, y);
 
 	//예제 21-14 임시 객체를 전달하기
-	Area(Point(x, y));
+	// Area(Point(x, y));
 
 
 	return 0;
