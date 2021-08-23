27bde98c9578d70cc2e936c3d6fad1648461f1ac
yongmin01 <78396941+yongmin01@users.noreply.github.com>
2021-08-23 16:41:41 +0900

    brainCpp_ch21-16
    접근제어

---
+++ brainCpp_ch21-16/brainCpp_ch21-16/brainCpp_ch21-16.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+class AccessControl
+{
+public:
+	char publicData;
+	void publicFunc() {}
+
+protected:
+	int protectedData;
+	void protectedFunc() {}
+	
+private :
+	float privateData;
+	void privateFunc() {}
+
+public:
+	void AccessAllMembers();
+};
+
+void AccessControl::AccessAllMembers()
+{
+	publicData = 'A';
+	publicFunc();
+	protectedData = 100;
+	protectedFunc();
+	privateData = 4.5f;
+	privateFunc();
+}
+
+
+int main()
+{
+	AccessControl ac;
+
+	ac.AccessAllMembers();
+
+	return 0;
+}
