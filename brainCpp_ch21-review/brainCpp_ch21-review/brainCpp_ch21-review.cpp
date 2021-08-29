d9dd05bb484d1bc48407acdf665e97757ffa2f1a
yongmin01 <78396941+yongmin01@users.noreply.github.com>
2021-08-29 18:59:13 +0900

    brainCpp_ch21-review
    ch21 이것만은 알고 갑시다 #2

---
+++ brainCpp_ch21-review/brainCpp_ch21-review/brainCpp_ch21-review.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+
+class Dummy
+{
+public:
+	string s1 = "생성자";
+	string s2 = "복사 생성자";
+
+	Dummy();
+	Dummy(const Dummy& dm);
+	~Dummy();
+
+};
+Dummy::Dummy()
+{
+	cout << s1 << endl;
+}
+Dummy::Dummy(const Dummy& dm)
+{
+	cout << dm.s2 << endl;
+}
+Dummy::~Dummy()
+{
+	cout << "소멸자" << endl;
+}
+int main()
+{
+	cout << "d1 생성" << endl;
+	Dummy d1;
+	cout << "d2 생성" << endl;
+	Dummy d2 = d1;
+	cout << "끝" << endl;
+	return 0;
+}
